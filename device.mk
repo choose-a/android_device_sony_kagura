3fa651ab641ff519c0a517d9a4a2a1ebab4237a0
Erik Castricum <git@cas-online.nl>
2019-10-01 18:00:51 +0200

    Revert "kagura: add vendor props"
    This reverts commit 253cad89821ee7e5e6c989913fc89a269ec50415.

--- device.mk
+++ device.mk
@@ -76,8 +76,5 @@ $(call inherit-product-if-exists, vendor/broadcom/wlan/bcmdhd/firmware/bcm4359/d
 # copy NFC firmware
 $(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
 
-# Include vendor props
-include device/sony/kagura/vendor_prop.mk
-
 # include board vendor blobs
 $(call inherit-product-if-exists, vendor/sony/kagura/kagura-vendor.mk)
